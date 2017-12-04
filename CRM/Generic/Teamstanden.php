7129f6b971ec23100453d0eefac4a2ce2d71be96
Jaap Jansma <jaap@edeveloper.nl>
2017-12-04 11:38:17 +0100

    Fixed #2176: totaalstand roparun in api en rapport komen niet overeen.

--- CRM/Generic/Teamstanden.php
+++ CRM/Generic/Teamstanden.php
@@ -42,6 +42,41 @@ class CRM_Generic_Teamstanden {
 	 * 	The ID of the campaign.
 	 * @return float
 	 */
+	public static function getTotalAmountForRoparun($campaign_id) {
+		try {
+			$config = CRM_Generic_Config::singleton();
+		
+			$financialTypeIds[] = $config->getDonatieFinancialTypeId();
+			$financialTypeIds[] = $config->getCollecteFinancialTypeId();
+			$financialTypeIds[] = $config->getLoterijFinancialTypeId();
+		
+			$sql = "
+				SELECT SUM(total_amount) 
+				FROM civicrm_contribution
+				LEFT JOIN `{$config->getDonatedTowardsCustomGroupTableName()}` donated_towards ON donated_towards.entity_id = civicrm_contribution.id
+				WHERE (donated_towards.`{$config->getTowardsTeamCustomFieldColumnName()}` IS NULL OR donated_towards.`{$config->getTowardsTeamCustomFieldColumnName()}` = 0)
+				AND civicrm_contribution.campaign_id = %1
+				AND civicrm_contribution.is_test = 0
+				AND civicrm_contribution.financial_type_id IN (" . implode(",", $financialTypeIds) . ")
+				AND civicrm_contribution.contribution_status_id = %2";
+			$params[1] = array($campaign_id, 'Integer');
+			$params[2] = array($config->getCompletedContributionStatusId(), 'Integer');
+			return (float) CRM_Core_DAO::singleValueQuery($sql, $params);
+		} catch (Exception $e) {
+			CRM_Core_Error::createError('Could not calculate the total amount', 800, 'Warning');
+			return 0.00;
+		}
+	}
+	
+	/**
+	 * Returns the total amount donated for a campaign
+	 * 
+	 * @param int $team_id
+	 * 	The contact id of the team
+	 * @param int $campaign_id
+	 * 	The ID of the campaign.
+	 * @return float
+	 */
 	public static function getTotalAmountDonatedForRoparun($campaign_id) {
 		try {
 			$config = CRM_Generic_Config::singleton();
